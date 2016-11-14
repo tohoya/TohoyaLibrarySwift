2440df924b6470880aab3938baa2f9b6aea84979
RED <red@intcompany.co.kr>
2016-11-14 15:03:14 +0900

    update
    update

--- tohoyaLibrary/tohoyaLibrary.h
+++ tohoyaLibrary/tohoyaLibrary.h
@@ -14,8 +14,13 @@ FOUNDATION_EXPORT double tohoyaLibraryVersionNumber;
 //! Project version string for tohoyaLibrary.
 FOUNDATION_EXPORT const unsigned char tohoyaLibraryVersionString[];
 
+//! Project version number for Reachability.
+FOUNDATION_EXPORT double ReachabilityVersionNumber;
+
+//! Project version string for Reachability.
+FOUNDATION_EXPORT const unsigned char ReachabilityVersionString[];
+
 // In this header, you should import all the public headers of your framework using statements like #import <tohoyaLibrary/PublicHeader.h>
 
 #import <tohoyaLibrary/MBProgressHUDForSwift-Bridging-Header.h>
 //#import <CommonCrypto/CommonCrypto.h>
-
